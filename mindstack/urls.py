b0bab47069a41ac0184d7ec7d237435979518e59
Syafiq <syafiq@syafiqkay.com>
2025-07-21 14:49:35 +0000

    feat: build a new homepage to reduce clutter

--- mindstack/urls.py
+++ mindstack/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('progress.urls')),
+    path('', include('home.urls')),
+    path('progress/', include('progress.urls')),
 ]
