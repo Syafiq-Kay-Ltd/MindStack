73359f2153b92902c2cc96a7cba4ef62cf857eb1
Syafiq <syafiq@syafiqkay.com>
2025-07-22 22:16:49 +0100

    refactor: update models.py

--- home/models.py
+++ home/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
-# project category
+# model for project category: cat_id (PK), title, description
+
+# model for project: project_id (PK), cat_id (FK), title, description, created_at, updated_at, status
+
+# model for tag: probably links to cortexdb tag model TBC
 
